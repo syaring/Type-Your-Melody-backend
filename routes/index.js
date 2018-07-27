5ea1d37e9a6b2c51eb4ea3b9fc382f1c6a87afd1
syaring <sarah.k.serim@gmail.com>
2018-07-27 13:24:56 +0900

    초기설정 및 pdf 수신 후 서버 동일폴더에 저장

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
+
+var fs = require('file-system');
+var PDFDoc = require('pdfkit');
+var SVGtoPDF = require('svg-to-pdfkit');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', cors(), function(req, res, next) {
+  res.render('index', { title: 'Expsress' });
+});
+
+router.post('/toPDF', cors(), function(req, res, next) {
+  var doc = new PDFDoc();
+  var stream = fs.createWriteStream('notes.pdf');
+  var svg = req.body.tagData;
+
+  SVGtoPDF(doc, svg, 0, 0);
+
+  stream.on('finish', function() {
+    console.log(fs.readFileSync('notes.pdf'))
+  });
+
+  doc.pipe(stream);
+  doc.end();
+  // console.log(req.body.tagData);
+
+  res.status(200).json({
+    msg: "done"
+  });
+});
+
+router.options('/toPDF', cors(), function (req, res, next) {
+  res.status(200).end();
 });
 
 module.exports = router;
